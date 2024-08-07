28bdf92e2cdd5fbf7a3838bb37595ca615b3a6e3
Frederich Blessy <frederichblessy@gmail.com>
2024-08-07 19:31:43 +0700

    update prompt

--- handler/system_prompt.go
+++ handler/system_prompt.go
@@ -23,6 +23,5 @@ var systemPrompt = `given receipt array, parse to this schema:
 - otherPayments should not have "total" name.
 - Ignore names/prices that seem strange or do not look like product names, or numbers that do not make sense.
 - In Indonesia, tax usually named "PPN", "Pajak", "PB", or "PB1" and discount usually named "Diskon" with code or no code.
-- When there is tax or discount or Service charge or SC, they're not always use percentage, if the amount is greater than 100, get percentage from (tax amount / grand total from items +- otherPayments) * 100, otherwise get the exact value.
-- Other payments type other than tax or discount & SC, get the exact value instead of percentage.
+- Other payments is not always use percentage. If it is, use percentage as true, otherwise get the exact value instead of percentage.
 `
